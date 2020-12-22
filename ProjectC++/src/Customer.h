0d7825634a8e303c04a6bcc5a1de47ac83899cf9
therealyo <72134455+therealyo@users.noreply.github.com>
2020-12-22 02:21:59 +0200

    Update Customer.h

--- ProjectC++/src/Customer.h
+++ ProjectC++/src/Customer.h
@@ -10,24 +10,24 @@ private:
 	double sum;
 
 public:
-	Customer();
-	Customer(std::string& name, double time, double sum);
-	Customer(std::string& pers);
+	Customer(); // Коструктор за замовчуванням
+	Customer(std::string& name, double time, double sum); // Конструктор для вводу даних
+	Customer(std::string& pers); // Конструктор для конвертації рядку у об'єкт класу
 	
-	std::string GetName();
+	std::string GetName(); // методи геттери
 	double GetTime();
 	double GetSum();
 	
-	void SetName(std::string& name);
+	void SetName(std::string& name); // методи сеттери
 	void SetTime(double time);
 	void SetSum(double sum);
 	
-	bool operator > (Customer& pers);
+	bool operator > (Customer& pers); // перевантаження булевих операторів
 	bool operator < (Customer& pers);
 	bool operator ==(Customer& pers);
 
-	friend std::ostream& operator << (std::ostream& os, const Customer& pers);
-	friend std::istream& operator >> (std::istream& is, Customer& cust);
-	friend bool operator < (const Customer& cust1, const Customer& cust2);
+	friend std::ostream& operator << (std::ostream& os, const Customer& pers); // перевантаження потоку виводу
+	friend std::istream& operator >> (std::istream& is, Customer& cust); // перевантаження потоку вводу
+	friend bool operator < (const Customer& cust1, const Customer& cust2); // булева функція порівняння
 };
 
